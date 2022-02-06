d6cce660d36bde899ab1d7d3bb3238df5336ae09
Suman Mandal <sumanmandal15135@gmail.com>
2022-02-07 00:52:50 +0530

    Update ContainsDuplicate.cpp
    solution using set

--- ContainsDuplicate.cpp
+++ ContainsDuplicate.cpp
@@ -1,5 +1,6 @@
 //217. Contains Duplicate
 
+//using unordered map
 
 class Solution {
 public:
@@ -8,6 +9,19 @@ public:
         for(int i=0; i<nums.size(); i++){
             data[ nums[i] ] += 1;
             if( data[nums[i]] > 1 ) return true;
+        } return false;
+    }
+};
+
+//using set solution 2
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> st;
+        for(int& num : nums){
+            if( st.find(num) != st.end() ) return true;
+            st.insert(num);
         } return false;
     }
 };
